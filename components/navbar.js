9fefa89db2a41a816c4c0d42cf5f78ecb2104f26
flatcafe <bochiclu@gmail.com>
2026-06-02 17:10:54 +0900

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -2,15 +2,15 @@
 class CafeNav extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-      <nav class="bg-white border-t border-gray-200 flex justify-between items-center px-4 py-2 z-20 font-nunito shrink-0 pb-safe">
+      <nav class="bg-[#DFC6B0] border-t border-[#c9b4a1] flex justify-between items-center px-4 py-2 z-20 font-nunito shrink-0 pb-safe">
         <a href="index.html" class="flex flex-col items-center w-14">
           <img src="images/today.png" alt="Today" class="w-12 h-12 object-contain">
-          <span class="text-[12px] font-bold text-gray-600">Today</span>
+          <span class="text-[12px] font-bold text-[#5a4a42] mt-1">Today</span>
         </a>
         
         <a href="calendar.html" class="flex flex-col items-center w-14">
           <img src="images/calender.png" alt="Calender" class="w-12 h-12 object-contain">
-          <span class="text-[12px] font-bold text-gray-600">Calender</span>
+          <span class="text-[12px] font-bold text-[#5a4a42] mt-1">Calender</span>
         </a>
         
         <a href="index.html" class="flex flex-col items-center w-16">
@@ -19,12 +19,12 @@ class CafeNav extends HTMLElement {
 
         <a href="memo.html" class="flex flex-col items-center w-14">
           <img src="images/memo.png" alt="Memo" class="w-12 h-12 object-contain">
-          <span class="text-[12px] font-bold text-gray-600">Memo</span>
+          <span class="text-[12px] font-bold text-[#5a4a42] mt-1">Memo</span>
         </a>
 
         <a href="setting.html" class="flex flex-col items-center w-14">
           <img src="images/setting.png" alt="Setting" class="w-12 h-12 object-contain">
-          <span class="text-[12px] font-bold text-gray-600">Setting</span>
+          <span class="text-[12px] font-bold text-[#5a4a42] mt-1">Setting</span>
         </a>
       </nav>
     `;
