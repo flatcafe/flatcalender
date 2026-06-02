3dfb162dfbfd3eaf05b7b364ae393b659cda98ab
flatcafe <bochiclu@gmail.com>
2026-06-02 21:41:03 +0900

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,8 +1,7 @@
-// components/navbar.js
 class CafeNav extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-      <nav class="bg-[#DFC6B0] border-t border-[#c9b4a1] flex justify-between items-center px-4 py-2 z-20 font-nunito shrink-0 pb-safe">
+      <nav class="bg-[#DFC6B0] border-t border-[#c9b4a1] flex justify-between items-center px-4 py-2 z-50 font-nunito shrink-0 pb-safe relative">
         <a href="index.html" class="flex flex-col items-center w-14">
           <img src="images/today.png" alt="Today" class="w-12 h-12 object-contain">
           <span class="text-[12px] font-bold text-[#5a4a42] mt-1">Today</span>
@@ -13,7 +12,7 @@ class CafeNav extends HTMLElement {
           <span class="text-[12px] font-bold text-[#5a4a42] mt-1">Calender</span>
         </a>
         
-        <a href="index.html" class="flex flex-col items-center w-16">
+        <a href="index.html" id="nav-logo" class="flex flex-col items-center w-16 outline-none focus:outline-none">
           <img src="images/logo.png" alt="Logo" class="w-16 h-16 object-contain">
         </a>
 
@@ -30,5 +29,4 @@ class CafeNav extends HTMLElement {
     `;
   }
 }
-
 customElements.define('cafe-nav', CafeNav);
