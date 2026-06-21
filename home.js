c3608b0f16a115f17ce3d0501758632c054c7375
flatcafe <bochiclu@gmail.com>
2026-06-21 11:52:23 +0900

    Update home.js

--- home.js
+++ home.js
@@ -1,1 +1,13 @@
-
+import { auth, db } from './firebase-config.js';
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import {
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  getDocs,
+  query,
+  where,
+  updateDoc,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
