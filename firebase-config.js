522d90f67c94a75cfa78764103d8bef1e1635428
flatcafe <bochiclu@gmail.com>
2026-06-24 20:48:16 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { getMessaging } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging.js";
 
 // ！！！ここに自分の設定を貼り付けてください！！！
 const firebaseConfig ={
