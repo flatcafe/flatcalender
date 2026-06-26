0c36f883664053e0c96775ef6019585dcd0fdd90
flatcafe <bochiclu@gmail.com>
2026-06-26 21:28:45 +0900

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,8 @@ exports.sendQueuedNotifications = onSchedule(
   "every 1 minutes",
   async () => {
 
+    console.log("通知チェック");
+
     const now = new Date();
 
     const snapshot = await admin.firestore()
@@ -15,51 +17,23 @@ exports.sendQueuedNotifications = onSchedule(
       .where("sendAt", "<=", now)
       .get();
 
-
     if (snapshot.empty) {
-      console.log("通知なし");
       return null;
     }
-
-
-    const usersSnapshot = await admin.firestore()
-      .collection("users")
-      .get();
 
 
     for (const docSnap of snapshot.docs) {
 
       const data = docSnap.data();
 
-      const tokens = [];
-
-      usersSnapshot.forEach(userDoc => {
-        const userData = userDoc.data();
-
-        if (userData.fcmToken) {
-          tokens.push(userData.fcmToken);
-        }
-      });
-
-
-      if (tokens.length > 0) {
-
-        await admin.messaging().sendEachForMulticast({
-          notification: {
-            title: "ふらっとCafe",
-            body:
-              `${data.author}さんが予定を${data.count}件追加しました`
-          },
-          tokens
-        });
-
-      }
+      console.log(
+        `${data.count}件処理`
+      );
 
 
       await docSnap.ref.update({
         sent: true
       });
-
     }
 
 
