0911a1e506d96d1daada732061bfabb59cca01b6
flatcafe <bochiclu@gmail.com>
2026-06-28 13:16:55 +0900

    Update home.js

--- home.js
+++ home.js
@@ -182,7 +182,7 @@ await addDoc(
     type: "bubble",
     author: document.getElementById('display-user-name').textContent,
     authorUid: auth.currentUser.uid,
-    authorIcon: '',
+    characterName: name, // ⭕ 空文字から name に変更
     title: "吹き出しを変更しました",
     createdAt: new Date().toISOString()
   }
@@ -194,7 +194,7 @@ await addDoc(
     type: "bubble",
     count: 1,
     author: document.getElementById('display-user-name').textContent,
-    icon: '',
+    icon: name ? `images/${name}.png` : '', // ⭕ 空文字から画像パスに変更
     sent: false,
     createdAt: new Date().toISOString(),
     sendAt: new Date(Date.now() + 60 * 1000)
