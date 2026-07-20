7c21369b1466c945ff1c6be7d83df038b24ff2b8
flatcafe <bochiclu@gmail.com>
2026-07-20 11:43:51 +0900

    Update memo-v2.js

--- memo-v2.js
+++ memo-v2.js
@@ -1,1 +1,25 @@
-import { auth, db } from "./firebase-config.js";
+// ================================
+// Firebase
+// ================================
+
+import { db, auth } from "./firebase-config.js";
+
+import {
+  collection,
+  query,
+  orderBy,
+  limit,
+  onSnapshot,
+  doc,
+  updateDoc,
+  deleteDoc,
+  arrayUnion,
+  getDoc,
+  addDoc,
+  serverTimestamp,
+  where
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
+// ================================
+// DOM
+// ================================
